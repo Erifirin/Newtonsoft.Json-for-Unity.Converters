3b01bf21e26636c85e3d92f568f55d5fe25ad192
George Gromov <erifirin@gmail.com>
2025-03-20 11:15:17 +0300

    added summary to HideInJsonConverterSettings (draft)

--- Packages/Newtonsoft.Json-for-Unity.Converters/UnityConverters/HideInJsonConverterSettings.cs
+++ Packages/Newtonsoft.Json-for-Unity.Converters/UnityConverters/HideInJsonConverterSettings.cs
@@ -2,6 +2,10 @@ using System;
 
 namespace Newtonsoft.Json.UnityConverters
 {
+    /// <summary>
+    /// The attribute allows to hide JsonConverter from Json.Net converters settings
+    /// and exclude it from auto sync process.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class HideInJsonConverterSettings : Attribute { }
 }
